5f6701b4ac9d8fa4c492373420b2484109ed0451
Shelby Jorgensen <126823300+ShelbyJorgensen@users.noreply.github.com>
2023-04-06 11:34:50 -0700

    Update CS380_Lab1.java
    Updated the program outputs

--- CS380_Lab1.java
+++ CS380_Lab1.java
@@ -3,10 +3,10 @@ package lab1;
 public class CS380_Lab1 {
 	public static void main(String[] args) {
 		int givenNum = 12345;
+		System.out.println("Given Number: " + givenNum);
 		StringBuilder str = new StringBuilder(Integer.toString(givenNum));
 		str.reverse();
 		int reverseNum = Integer.parseInt(str.toString());
-		System.out.println(reverseNum);
+		System.out.println("Reversed Number: " + reverseNum);
 	}
 }
-
